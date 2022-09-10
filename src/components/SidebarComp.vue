04b1ed1677d537970bb8b2e82691045fa29bcd84
AMYR <info@amyr.ir>
2022-09-10 16:48:44 +0430

    refactor: images

--- src/components/SidebarComp.vue
+++ src/components/SidebarComp.vue
@@ -5,7 +5,7 @@
 <template>
   <aside class="min-w-[300px] w-[300px] h-full">
     <div class="bg-background-secondary shadow-xl relative p-8 rounded-3xl transition-shadow duration-300 ease-in-out">
-        <img class="w-[180px] h-[180px] -mt-32 mb-8 mx-auto rounded-3xl" src="https://lh3.googleusercontent.com/a-/AFdZuco2ydeYP80UR5yK9RrXrG-lRiFaYK6BkXdWg4qavQ=s180-p" />
+        <img class="w-[180px] h-[180px] -mt-32 mb-8 mx-auto rounded-3xl" src="/me.jpg" />
         <div class="flex flex-col gap-7 justify-center items-start">
             <div class="w-full text-center flex flex-col justify-center items-center gap-4">
                 <h3 class="text-2xl"><span class="font-bold">امیرحسین</span> حسن زاده</h3>
